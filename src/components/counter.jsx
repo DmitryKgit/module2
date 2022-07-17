9bc15d7034e918eadeafe923bd8721472a4f25ae
Dmitriy Da4a <kaprandm@gmail.com>
2022-07-17 05:16:54 +0300

    lesson2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,51 +1,36 @@
 import React, {useState} from "react"
 
-const Counter = () => {
-    const [count, setCount] = useState(0)
-    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
+const Counter = (props) => {
+    const {value} = props
 
-    const formatCount = () => {
-        return count === 0 ? <h1>empty</h1> : count
+    const formatValue = () => {
+        return value === 0 ? <h1>empty</h1> : value
     }
     
     const getBadgeClasses = () => {
         let classes = 'badge m-2 '
-        classes += count === 0 ? 'bg-warning' : 'bg-primary'
+        classes += value === 0 ? 'bg-warning' : 'bg-primary'
         return classes
     }
 
     const handleIncrement = () => {
-        setCount((prevState) => prevState + 1)
+        console.log('+')
+        // setValue((prevState) => prevState + 1)
     }
 
     const handleDecrement = () => {
-        setCount((prevState) => prevState - 1)
-    }
-
-    const handleTagChange = (id) => {
-        setTags(prevState => prevState.filter(tag => tag !== id))
-    }
-
-    const renderTags = () => {
-        return tags.length !== 0 ? tags.map(tag => 
-            <li
-                key={tag}
-                className="btn btn-primary btn-sm m-2"
-                onClick={() => handleTagChange(tag)}
-            >
-                {tag}
-            </li>) : "No tags"
+        console.log('-')
+        // setValue((prevState) => prevState - 1)
     }
 
     return (
-        <>
-            <ul>
-                {renderTags()}
-            </ul>
-            <span className={getBadgeClasses()}>{formatCount()}</span>
+        <div>
+            <span>{props.name}</span>
+            <span className={getBadgeClasses()}>{formatValue()}</span>
             <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
             <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>-</button>
-        </>
+            <button className="btn btn-danger btn-sm m-2" onClick={() => props.onDelete(props.id)}>Delete</button>
+        </div>
     )
 }
 
